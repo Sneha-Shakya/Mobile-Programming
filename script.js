26263a4b1bbd3bcba425af52bcb3d40431d736c2
Sneha-Shakya <shkyasneha@gmail.com>
2026-01-17 21:29:17 +0545

    Add bulb on and off

---
+++ script.js
@@ -0,0 +1,37 @@
+function checkResult() {
+  let totalMarks =
+    Number(document.getElementById("s1").value) +
+    Number(document.getElementById("s2").value) +
+    Number(document.getElementById("s3").value) +
+    Number(document.getElementById("s4").value) +
+    Number(document.getElementById("s5").value) +
+    Number(document.getElementById("s6").value) +
+    Number(document.getElementById("s7").value) +
+    Number(document.getElementById("s8").value);
+
+  let totalBox = document.getElementById("total");
+  totalBox.innerHTML = "Total Marks: " + totalMarks + " / 800";
+
+  let resultBox = document.getElementById("result");
+
+  if (totalMarks > 700) {
+    resultBox.innerHTML = "Distinction";
+    resultBox.style.color = "green";
+  } 
+  else if (totalMarks >= 600) {
+    resultBox.innerHTML = "First Division";
+    resultBox.style.color = "blue";
+  } 
+  else if (totalMarks >= 500) {
+    resultBox.innerHTML = "Second Division";
+    resultBox.style.color = "orange";
+  } 
+  else if (totalMarks >= 400) {
+    resultBox.innerHTML = "Third Division";
+    resultBox.style.color = "brown";
+  } 
+  else {
+    resultBox.innerHTML = "Fail";
+    resultBox.style.color = "red";
+  }
+}
